bcd9cf2a63bf7631c1c853c57e4f52c073f95e8e
twarped <samthebeastofall>
2024-01-02 16:36:38 -0700

    Added a bunch of files

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const readline = require('node:readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const engine = require('./engine');
 
-// Game it has to "learn" to play. I suck at this, and all this stuff is going to be from scratch, I really have no idea how to do any of this crap.
+const handlers = {
+    '404': error => {
+        return new Response("404 Not Found\n", { status: 404, statusText: "Not Found" });
+    },
+    '': req => {
+        return new Response(Bun.file('views/index.html'));
+    }
+}
 
-const s1 = 1; // 01 (2-bits) scenario 1
-const s11 = 5 // 0101 (4-bits) scenario 1 sub scenario 1
-const s12 = 6 // 0110 (4-bits) scenario 1 sub scenario 2
-const s13 = 7 // 0111 (4-bits) scenario 1 sub scenario 3
 
-const s2 = 2; // 10 (2-bits) scenario 2
-const s21 = 9 // 1001 (4-bits) scenario 2 sub scenario 1
-const s22 = 10 // 1010 (4-bits) scenario 2 sub scenario 2
-const s23 = 11 // 1011 (4-bits) scenario 2 sub scenario 3
 
-const s3 = 3; // 11 (2-bits) scenario 3
-const s31 = 13 // 1101 (4-bits) scenario 3 sub scenario 1
-const s32 = 14 // 1110 (4-bits) scenario 3 sub scenario 2
-const s33 = 15 // 1111 (4-bits) scenario 3 sub scenario 3
-
-const data = new Int8Array(32);
-console.log(data)
-
+Bun.serve({
+    async fetch(req) { // Request handler function to dish out requests
+        const url = new URL(req.url);
+        const pathname = url.pathname.substring(1);
+        
+        let handler = handlers[pathname] || Bun.file('public/' + pathname); // Try to find a handler for the requested path, either predifined or by the filename
+        if (typeof handler === 'function') 
+            return handler(req);
+        const exists = await handler.exists();
+        if (exists)
+            return new Response(handler);
+        return handlers['404']();
+    },
+    port: process.env.PORT || 3000
+})
